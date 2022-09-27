ef00d953a57f92d16edac2a1e8ff1389bd240eef
musgan <muhusgan@gmail.com>
2022-09-27 14:23:03 +0700

    update :  v1. tampilan halaman master project

--- TimesheetGeneratorApp/Models/MasterProjectModel.cs
+++ TimesheetGeneratorApp/Models/MasterProjectModel.cs
@@ -1,18 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TimesheetGeneratorApp.Models
 {
     public class MasterProjectModel
     {
         public int id { get; set; }
-
+        [DisplayName("Nama Project")]
         public string name { get; set; }
-
+        [DisplayName("Project ID")]
         public string project_id { get; set; }
+        [DisplayName("Version control")]
         public string version_control { get; set; }
+        [DisplayName("Host url")]
         public string host_url { get; set; }
+        [DisplayName("Akses token")]
         public string? accsess_token { get; set; }
+        [DisplayName("Username")]
         public string? username { get; set; }
+        [DisplayName("Password")]
         public string? password { get; set; }
     }
 }
