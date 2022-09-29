2843dac3a42908f4e121df8b266e45938b03afa9
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-09-29 15:04:33 +0700

    update: create service & model class for HariLibur

---
+++ TimesheetGeneratorApp/Models/HariLiburModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimesheetGeneratorApp.Models
+{
+    public class HariLiburModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime? holiday_date { get; set; }
+        public string holiday_name { get; set; }
+        public bool is_national_holiday { get; set; }   
+    }
+}
---
+++ TimesheetGeneratorApp/Services/HariLiburService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using TimesheetGeneratorApp.Models;
+
+namespace TimesheetGeneratorApp.Services
+{
+    public class HariLiburService
+    {
+        public string url;
+        public HttpClient httpClient;
+
+        public HariLiburService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            this.url = "https://api-harilibur.vercel.app/";
+        }
+
+        public List<HariLiburModel> getListByYear(string year)
+        {
+            var response = httpClient.GetAsync(url + "api?year=" + year).Result;
+            var responseMessage = response.Content.ReadAsStringAsync().Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var res = response.Content;
+                string rawData = res.ReadAsStringAsync().Result;
+
+                return JsonConvert.DeserializeObject<List<HariLiburModel>>(rawData);
+            }
+
+            throw new HttpRequestException(responseMessage);
+        }
+    }
+}
