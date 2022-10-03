71022abbb7f913a8ed591ee567c33c65c8cb3ac4
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-10-03 13:58:32 +0700

    update: add new migration for table HariLibur & add new column on table CommitModel

---
+++ TimesheetGeneratorApp/Migrations/Commit/20220928082308_AddColumnAuthorEmailToTableCommitModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimesheetGeneratorApp.Migrations.Commit
+{
+    public partial class AddColumnAuthorEmailToTableCommitModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "author_email",
+                table: "CommitModel",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "author_email",
+                table: "CommitModel");
+        }
+    }
+}
---
+++ TimesheetGeneratorApp/Migrations/HariLibur/20220929095757_AddNewTableHariLibur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TimesheetGeneratorApp.Migrations.HariLibur
+{
+    public partial class AddNewTableHariLibur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HariLiburModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    holiday_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    holiday_name = table.Column<string>(type: "text", nullable: false),
+                    is_national_holiday = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HariLiburModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HariLiburModel");
+        }
+    }
+}
